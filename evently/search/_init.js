e30b41262f5638925742fa14e79d7abcc708f577
hardtke <david@hardtke.net>
2011-01-27 09:53:31 -0800

    display link and id when doc structure not define or incorrect in _init.js

--- evently/search/_init.js
+++ evently/search/_init.js
@@ -6,7 +6,8 @@ function () {
   if (limit == null) limit = 10;
   var skip = app.req.query.skip;
   if (skip == null) skip = 0;
-  $.getJSON(app.db.uri+"_search?default_field=text&include_docs=true&q="+query+"&skip="+skip+"&limit="+limit,function (data) {
+  if (query == null || query.length == 0) return;
+  $.getJSON(app.db.uri+"_search?default_field=all&include_docs=true&q="+query+"&skip="+skip+"&limit="+limit,function (data) {
 	      var start = data.offset + 1;
 	      var finish = data.offset + data.rows.length;
 	      var length = data.rows.length;
@@ -18,18 +19,25 @@ function () {
 		$(search).html(header);
 		for (i = 0; i < data.rows.length; i++) {
 		  var row = data.rows[i];
-		  var id = row._id;
+		  var id = row.id;
 		  var doc = row.doc;
-		  var r = {
-		    screen_name : doc.user.screen_name,
-		    name : doc.user.name,
-		    avatar : doc.user.profile_image_url,
-		    text : $.linkify(doc.text),
-		    id : row._id,
-		    created_at : doc.created_at
-		  };
-		  var out = "<li class=\'tweet\' id=\'" + r.id +"\'><img class=\'profile\' src=\'"+ r.avatar + "\' align=\'left\' width=\'36\' /><h3 class=\'screen_name\'><a target=\'_blank\' class=\'user\' href=\'http://twitter.com/" + r.screen_name + "\'>" + r.screen_name + "(" + r.name + ")</a></h3><p class=\'text\'>" + r.text + "</p><span class=\'created_at\'>"+ r.created_at + "</span><br class=\'clear\'></li>"; 
-		$(search).append(out);
+		  try {
+		    // modify this part to display your results.  This displays Twitter data
+		    var r = {
+		      screen_name : doc.user.screen_name,
+		      name : doc.user.name,
+		      avatar : doc.user.profile_image_url,
+		      text : $.linkify(doc.text),
+		      id : row._id,
+		      created_at : doc.created_at
+		    };
+		    var out = "<li class=\'tweet\' id=\'" + r.id +"\'><img class=\'profile\' src=\'"+ r.avatar + "\' align=\'left\' width=\'36\' /><h3 class=\'screen_name\'><a target=\'_blank\' class=\'user\' href=\'http://twitter.com/" + r.screen_name + "\'>" + r.screen_name + "(" + r.name + ")</a></h3><p class=\'text\'>" + r.text + "</p><span class=\'created_at\'>"+ r.created_at + "</span><br class=\'clear\'></li>"; 
+		    $(search).append(out);
+		  } catch (err) {
+		    // if there is an error above, just display the doc id and a link
+		    var out = "<li class=\'tweet\' id=\'" + id +"\'><a target=\'_blank\' href=\'" + uri + id + "\'>" + id + "</a></li>";
+		    $(search).append(out);
+		  }
 		}
 		if (total > finish) {
 		  var newQuery =  $.query.set("skip",skip + limit).toString();
